02653912ac7ac2c9cfcff76e038b45cad1840d69
ariinromeror <ariin.romeror@gmail.com>
2026-01-15 12:52:14 +0100

    suma interactiva con input

--- Test.Suma.py
+++ Test.Suma.py
@@ -1,3 +1,6 @@
 def suma(num1, num2):
     return num1 + num2
-print(suma(3,5))
+if __name__ == '__main__':
+    num1 = float(input('Ingrese el primer número: '))
+    num2 = float(input('Ingrese el segundo número: '))
+    print(suma(num1, num2))
